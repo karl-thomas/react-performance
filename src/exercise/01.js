4f75ca2194545dfd6b760bbbfa9187bb84295125
karl-thomas <karl.thomas925@gmail.com>
2020-10-27 05:57:41 -0500

    use react suspense to lazy load a component

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,19 +2,17 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
 
-// 🐨 use React.lazy to create a Globe component which using a dynamic import
-// to get the Globe component from the '../globe' module.
+const Globe = React.lazy(() => import('../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
+  const loadingGlobe = (
+    <React.Suspense fallback={<p>loading glerbe</p>}>
+      <Globe />
+    </React.Suspense>
+  )
   return (
     <div
       style={{
@@ -35,13 +33,10 @@ function App() {
         {' show globe'}
       </label>
       <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
+        {showGlobe ? loadingGlobe : null}
       </div>
     </div>
   )
 }
-// 🦉 Note that if you're not on the isolated page, then you'll notice that this
-// app actually already has a React.Suspense component higher up in the tree
-// where this component is rendered, so you *could* just rely on that one.
 
 export default App
