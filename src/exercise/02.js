35bd2f8722ae83de7d25a5ed709cb29d4c4b78cb
karl-thomas <karl.thomas925@gmail.com>
2020-10-30 05:20:47 -0500

    use the webworker to speed up the app

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,8 +3,8 @@
 
 import React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../filter-cities'
-import {useForceRerender} from '../utils'
+import {getItems} from '../workerized-filter-cities'
+import {useForceRerender, useAsync} from '../utils'
 
 function Menu({
   items,
@@ -59,8 +59,12 @@ function ListItem({
 function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
+  const {data: allItems, run} = useAsync({data: [], status: 'pending'})
 
-  const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
+
   const items = allItems.slice(0, 100)
 
   const {
